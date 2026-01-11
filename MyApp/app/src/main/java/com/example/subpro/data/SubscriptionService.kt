5d089a8aac8c422ea767db2fe25fe17577abe406
POLINA <polinaionina67@gmail.com>
2026-01-11 17:08:08 +0500

    изменения в сервере для записи подписок

--- MyApp/app/src/main/java/com/example/subpro/data/SubscriptionService.kt
+++ MyApp/app/src/main/java/com/example/subpro/data/SubscriptionService.kt
@@ -16,12 +16,15 @@ import okhttp3.Response
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.RequestBody.Companion.toRequestBody
 import java.io.IOException
+import org.json.JSONObject
 
 @SuppressLint("StaticFieldLeak")
 object SubscriptionService {
 
     private const val PREFS = "subscriptions_prefs"
     private const val KEY = "subscriptions"
+    private const val SERVER_URL =
+        "https://droopingly-troughlike-dedra.ngrok-free.dev/api/Subscriptions"
 
     private lateinit var context: Context
     private val gson = Gson()
@@ -33,41 +36,20 @@ object SubscriptionService {
         restoreIdCounter()
     }
 
-    fun update(subscription: Subscription) {
-        val list = getAllInternal()
-        val idx = list.indexOfFirst { it.id == subscription.id }
-        if (idx != -1) {
-            list[idx] = subscription
-            save(list)
-            sendSubscriptionEvent("UPDATE", subscription)
-        }
-    }
-
-    fun delete(id: Int) {
-        val list = getAllInternal()
-        val deleted = list.firstOrNull { it.id == id }
-        val newList = list.filterNot { it.id == id }
-        save(newList)
-
-        if (deleted != null) {
-            sendSubscriptionEvent("DELETE", deleted)
-        }
-    }
-
-    fun getById(id: Int): Subscription? = getAllInternal().firstOrNull { it.id == id }
+    data class SubscriptionTemplate(
+        val name: String,
+        val price: Double,
+        val period: SubscriptionPeriod
+    )
 
     fun addFromTemplate(template: SubscriptionTemplate) {
-        val list = getAllInternal()
-        val newSub = Subscription(
-            id = idCounter.getAndIncrement(),
+        add(
             name = template.name,
             price = template.price,
             startDate = LocalDate.now().toString(),
             period = template.period,
             notificationDaysBefore = 3
         )
-        list.add(newSub)
-        save(list)
     }
 
     fun add(
@@ -78,6 +60,7 @@ object SubscriptionService {
         notificationDaysBefore: Int
     ) {
         val list = getAllInternal()
+
         val newSub = Subscription(
             id = idCounter.getAndIncrement(),
             name = name,
@@ -86,10 +69,35 @@ object SubscriptionService {
             period = period,
             notificationDaysBefore = notificationDaysBefore
         )
+
         list.add(newSub)
         save(list)
-        sendSubscriptionEvent("CREATE", newSub)
+
+        sendMetric(newSub)
+
+        sendCreateToServer(newSub)
     }
+
+    fun update(subscription: Subscription) {
+        val list = getAllInternal()
+        val index = list.indexOfFirst { it.id == subscription.id }
+        if (index != -1) {
+            list[index] = subscription
+            save(list)
+
+            sendUpdateToServer(subscription)
+        }
+    }
+
+
+    fun delete(id: Int) {
+        val list = getAllInternal()
+        save(list.filterNot { it.id == id })
+        sendDeleteToServer(id)
+    }
+
+    fun getById(id: Int): Subscription? =
+        getAllInternal().firstOrNull { it.id == id }
 
     fun getAll(): List<Subscription> = getAllInternal()
 
@@ -113,36 +121,130 @@ object SubscriptionService {
     private fun prefs() =
         context.getSharedPreferences(PREFS, Context.MODE_PRIVATE)
 
-
-    data class SubscriptionTemplate(
-        val name: String,
-        val price: Double,
-        val period: SubscriptionPeriod
-    )
-
-    private fun sendSubscriptionEvent(
-        action: String,
-        subscription: Subscription?
-    ) {
+    private fun sendMetric(subscription: Subscription) {
         val prefs = context.getSharedPreferences("auth_prefs", Context.MODE_PRIVATE)
-
-        val token = prefs.getString("jwt_token", null) ?: return
-        val deviceId = prefs.getString("local_device_id", null) ?: return
+        val token = prefs.getString("jwt_token", null)
 
         val body = mapOf(
-            "action" to action,
-            "deviceId" to deviceId,
-            "subscription" to subscription
+            "name" to subscription.name,
+            "price" to subscription.price,
+            "period" to subscription.period.name,
+            "isAuthorized" to (token != null)
         )
 
         val json = gson.toJson(body)
+        val requestBody = json.toRequestBody("application/json".toMediaType())
 
+
+
+        val request = Request.Builder()
+            .url("https://droopingly-troughlike-dedra.ngrok-free.dev/api/metrics")
+            .post(requestBody)
+            .build()
+
+        OkHttpClient().newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                response.close()
+            }
+        })
+    }
+
+    private fun sendCreateToServer(subscription: Subscription) {
+        val prefs = context.getSharedPreferences("auth_prefs", Context.MODE_PRIVATE)
+        val token = prefs.getString("jwt_token", null) ?: return
+
+        val body = mapOf(
+            "name" to subscription.name,
+            "price" to subscription.price,
+            "period" to subscription.period.name,
+            "nextPaymentDate" to subscription.startDate,
+            "notificationDaysBefore" to subscription.notificationDaysBefore
+        )
+
+        val json = gson.toJson(body)
         val requestBody = json.toRequestBody("application/json".toMediaType())
 
         val request = Request.Builder()
-            .url("https://ТВОЙ_СЕРВЕР/api/subscriptions/event")
+            .url(SERVER_URL)
             .addHeader("Authorization", "Bearer $token")
             .post(requestBody)
+            .build()
+
+        OkHttpClient().newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val body = response.body?.string() ?: return
+
+                try {
+                    val json = JSONObject(body)
+                    val serverId = json.getLong("id").toInt()
+
+                    val list = getAllInternal()
+
+                    val index = list.indexOfFirst { it.id == subscription.id }
+                    if (index != -1) {
+                        list[index] = subscription.copy(id = serverId)
+                        save(list)
+                    }
+
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                } finally {
+                    response.close()
+                }
+            }
+
+        })
+    }
+
+    private fun sendUpdateToServer(subscription: Subscription) {
+        val prefs = context.getSharedPreferences("auth_prefs", Context.MODE_PRIVATE)
+        val token = prefs.getString("jwt_token", null) ?: return
+
+        val body = mapOf(
+            "name" to subscription.name,
+            "price" to subscription.price,
+            "period" to subscription.period.name,
+            "nextPaymentDate" to subscription.startDate,
+            "notificationDaysBefore" to subscription.notificationDaysBefore
+        )
+
+        val json = gson.toJson(body)
+        val requestBody = json.toRequestBody("application/json".toMediaType())
+
+        val request = Request.Builder()
+            .url("$SERVER_URL/${subscription.id}")
+            .addHeader("Authorization", "Bearer $token")
+            .put(requestBody)
+            .build()
+
+        OkHttpClient().newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                response.close()
+            }
+        })
+    }
+
+
+    private fun sendDeleteToServer(subscriptionId: Int) {
+        val prefs = context.getSharedPreferences("auth_prefs", Context.MODE_PRIVATE)
+        val token = prefs.getString("jwt_token", null) ?: return
+
+        val request = Request.Builder()
+            .url("$SERVER_URL/$subscriptionId")
+            .addHeader("Authorization", "Bearer $token")
+            .delete()
             .build()
 
         OkHttpClient().newCall(request).enqueue(object : Callback {
